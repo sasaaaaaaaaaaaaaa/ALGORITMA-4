637921d77044ed95eae0371fae89e17109024e5b
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 12:45:28 +0700

    membuat for loop

--- 10 for loop dengan berbagai ekspresi.php
+++ 10 for loop dengan berbagai ekspresi.php
@@ -2,15 +2,15 @@
 /* 
 Contoh 1, bentuk lengkap, SERING DAN UMUM DIGUNAKAN
 */
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 0; $i <= 15; $i++) {
     echo $i;
 }
 echo "<hr>";
 /*
 Contoh 2, dengan ekspresi2 kosong, kode dihentikan dengan brak statement 
 */
-for ($i = 1;; $i++) {
-    if ($i > 10) {
+for ($i = 0;; $i++) {
+    if ($i > 15) {
         break;
     }
     echo $i;
@@ -19,9 +19,9 @@ echo "<hr>";
 /* 
 Contoh 3, semua ekspresi kosong
 */
-$i = 1;
+$i = 0;
 for (;;) {
-    if ($i > 10) {
+    if ($i > 15) {
         break;
     }
     echo $i;
@@ -31,6 +31,6 @@ echo "<hr>";
 /* 
 Contoh 4, tanpa statement hanya ekspresi saja
  */
-for ($i = 1; $i <= 10; print $i, $i++);
+for ($i = 0; $i <= 15; print $i, $i++);
 echo "<hr>";
-
+print_r("<p><strong>by :alika naswa sabilaa");
