d049001d31ba4ad0e7057678b82bd22f2a8e500a
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 12:51:12 +0700

    membuat for loop nilai tertentu

---
+++ 11 For Loop Pada Nilai Tertentu.php
@@ -0,0 +1,10 @@
+<?php
+for ($i = 1; $i <= 15; $i++) {
+	if ($i == 5) {
+		continue;
+	}
+    echo $i; // 1 2 3 4 6 7 8 9 10 11 12 13 14 15
+    echo "<hr>";
+}
+print_r("<p><strong>by :alika naswa sabilaa");
+?>
