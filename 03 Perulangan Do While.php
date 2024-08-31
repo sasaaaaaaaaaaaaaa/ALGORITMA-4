89369f9e476c7a33d5608106fe37b1a792633009
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 11:47:49 +0700

    membuat perulangan foreach

---
+++ 03 Perulangan Do While.php
@@ -0,0 +1,12 @@
+<?php 
+
+$ulangi = 5;
+
+do {
+    
+    echo "<p>ini adalah perulangan ke-$ulangi</p><hr>";
+    $ulangi--;
+} while ($ulangi > 0);
+print_r("<p><strong>by :alika naswa sabila");
+?>
+
