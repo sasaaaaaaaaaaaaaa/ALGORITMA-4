a320537ac62f8659d109405b7e84cb46c4a69f9f
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 11:40:05 +0700

    membuat perulangan whiel

---
+++ 2 Perulangan While.php
@@ -0,0 +1,11 @@
+<?php 
+
+$ulangi = 0;
+
+while($ulangi < 10){
+    echo "<hr>";
+    echo "<p>Ini adalah perulangan ke-$ulangi</p>";
+    $ulangi++;
+}
+print_r("<p><strong>by :alika naswa sabila");
+?>
