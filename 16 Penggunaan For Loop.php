46e20e6b0ed5cb33f4564ff6ed50c7c4d1745b45
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 13:48:59 +0700

    penggunaan array

---
+++ 16 Penggunaan For Loop.php
@@ -0,0 +1,5 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . " ";
+}
+print_r("<p><strong>by :alika naswa sabilaa");
---
+++ 17 Penggunaan Pada Array.php
@@ -0,0 +1,8 @@
+<?php
+$arr = array("pisang", "anggur", "duren");
+
+for ($i = 0; $i < count($arr); $i++) {
+    echo $arr[$i] . "<hr> ";
+    
+}
+print_r("<p><strong>by :alika naswa sabilaa");
