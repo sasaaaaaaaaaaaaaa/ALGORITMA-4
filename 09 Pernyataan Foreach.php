d8ed88fc9a445848fc127dbc7876f07ed16db80b
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-31 12:42:11 +0700

    membuat penyataan foreach

--- 09 Pernyataan Foreach.php
+++ 09 Pernyataan Foreach.php
@@ -3,4 +3,4 @@ $warna = array('kuning', 'putih', 'hitam');
 foreach ($warna as $w) {
   echo "Warna: $w<hr>";
 }
-print_r("<p><strong>by :alika naswa sabila");
+print_r("<p><strong>by :alika naswa sabilaa");
