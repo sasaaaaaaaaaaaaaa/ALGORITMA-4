913e22d58893288109e29ceb9832728364425f28
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-02 00:27:31 +0700

    isi array

---
+++ 31 Menampilkan isi Array.php
@@ -0,0 +1,9 @@
+<?php
+// membuat array
+$barang = ["meja", "bangku", "Spidol"];
+
+// menampilkan isi array
+echo $barang[0]."<br><hr>";
+echo $barang[1]."<br><hr>";
+echo $barang[2]."<br><hr>";
+print_r("<p><strong>by :alika naswa sabilaa");
