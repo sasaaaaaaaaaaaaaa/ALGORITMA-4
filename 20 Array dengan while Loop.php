8a3da2212ac6c27457e620f5e6d25e235882f418
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-01 21:04:22 +0700

    array dengan while loop

---
+++ 20 Array dengan while Loop.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["jeruk", "pisang", "durian"];
+$i = 0;
+
+while ($i < count($fruits)) {
+    echo $fruits[$i] . "<br><hr>";
+    $i++;
+}
+print_r("<p><strong>by :alika naswa sabilaa");
+?>
