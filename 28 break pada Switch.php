761052bae7fd0f9ff4e27407e28130007697725b
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-02 00:12:09 +0700

    break pada switch

---
+++ 28 break pada Switch.php
@@ -0,0 +1,18 @@
+<?php
+$buah = "durian";
+
+switch ($buah) {
+    case "mangga":
+        echo "Ini mangga";
+        break;
+    case "durian":
+        echo "Ini durian";
+        // Break disini menghentikan switch
+        break;
+    case "pisang":
+        echo "Ini pisang";
+        break;
+    default:
+        echo "Buah tidak dikenal";
+}
+print_r("<p><strong>by :alika naswa sabilaa");
