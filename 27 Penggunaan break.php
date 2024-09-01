50afc28b01564e68effc59c8aad05d5d89d205d5
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-02 00:08:14 +0700

    penggunaan break

---
+++ 27 Penggunaan break.php
@@ -0,0 +1,10 @@
+<?php
+for ($i = 0; $i < 10; $i++) {
+    if ($i === 5) {
+        break;
+    }
+    echo $i . "<br> <hr>";
+}
+// Output: 0 1 2 3 4
+
+print_r("<p><strong>by :alika naswa sabilaa");
