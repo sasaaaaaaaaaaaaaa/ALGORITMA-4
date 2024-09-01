3fd37b4400696005ae2f490549c88c475374a598
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-01 23:51:33 +0700

    contoh key

---
+++ 25 Contoh dengan Key.php
@@ -0,0 +1,7 @@
+<?php
+$harga = array("apel" => 20000, "pisang" => 15000, "ceri" => 25000);
+
+foreach ($harga as $kunci => $nilai) {
+    echo "Harga dari " . $kunci . " adalah " . $nilai . "<br><hr>";
+}
+print_r("<p><strong>by :alika naswa sabilaa");
