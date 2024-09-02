fc5841a4fceb5b63ccb9c790ce989ee062f75fb4
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-02 16:01:28 +0700

    data array

---
+++ 42 PENGAYAAN ARRAY PHP.php
@@ -0,0 +1,10 @@
+<?php
+// Menggunakan fungsi array()
+$buah = array('apel', 'jeruk', 'pisang');
+
+// Menggunakan tanda kurung siku []
+$warna = ['merah', 'hijau', 'biru'];
+//Mengakses Data Array
+echo $buah[1]; // akan menampilkan 'jeruk'
+echo $warna[0]; // akan menampilkan 'merah'
+
