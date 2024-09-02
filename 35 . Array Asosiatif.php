641680302e3f54ebec515061063c136c06e77c76
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-09-02 15:29:06 +0700

    array asosiatif

---
+++ 35 . Array Asosiatif.php
@@ -0,0 +1,13 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul" => "Belajar Pemrograman PHP",
+    "penulis" => "alika naswa sabila",
+    "view" => 135
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+print_r("<p><strong>by :alika naswa sabilaa");
